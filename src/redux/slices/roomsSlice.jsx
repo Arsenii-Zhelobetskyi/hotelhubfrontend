d56a7d7656679b71d4981cfcfc77607158ca84f3
Kateryna Pazynych <pazynych_ka@st.znu.edu.ua>
2023-11-22 11:49:31 +0200

    working

--- src/redux/slices/roomsSlice.jsx
+++ src/redux/slices/roomsSlice.jsx
@@ -9,6 +9,15 @@ export const fetchOccupiedPlacesByHotel = createAsyncThunk(
   }
 );
 
+export const fetchRooms = createAsyncThunk(
+  "fetchRooms",
+  async () => {
+    const response = await fetch(`${API_URL}/api/rooms/`);
+    const data = await response.json();
+    return { data };
+  }
+)
+
 const roomsSlice = createSlice({
   name: "rooms",
   initialState: {
